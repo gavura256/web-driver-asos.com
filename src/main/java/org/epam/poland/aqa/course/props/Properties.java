902c0bc9d31d8dc72ff4e76a9d03e5a3f7385e44
Yuliia_Hrabovska <yuliia.hrabovska@regeneron.com>
2022-02-23 16:19:25 +0200

    Add properties

---
+++ src/main/java/org/epam/poland/aqa/course/props/Properties.java
@@ -0,0 +1,6 @@
+package org.epam.poland.aqa.course.props;
+
+public final class Properties {
+    public static final  String GIT_HUB_LOGIN = "juliaHrabovska";
+    public static final  String GIT_HUB_PASSWORD = "";
+}
